58fef32c9dc17f415f0c21d74989ffcaeae87bcb
Deepak Sharma <sharma.deepak8616@gmail.com>
2020-04-26 18:46:36 -0400

    review comments incorporated

--- src/test/java/Pages/BasePage.java
+++ src/test/java/Pages/BasePage.java
@@ -1,24 +1,26 @@
 package Pages;
 
+import com.google.common.base.Function;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.FluentWait;
+import org.openqa.selenium.support.ui.Wait;
 import stepDefinition.DriverWrapper;
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.Select;
 
 import java.sql.Driver;
+import java.time.Duration;
 import java.util.List;
 
 public class BasePage {
 
     public void type(By locator, String data) {
-        WebElement element = DriverWrapper.getDriver().findElement(locator);
+        WebElement element = findElement_fluentWait(locator);
         element.sendKeys(data);
     }
 
     public void clickOn(By locator) {
-        WebElement element = DriverWrapper.getDriver().findElement(locator);
+        WebElement element = findElement_fluentWait(locator);
         element.click();
     }
 
@@ -111,6 +113,38 @@ public class BasePage {
         js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
     }
 
+
+    public WebElement findElement_fluentWait(By locator) {
+        Wait fWait = new FluentWait(DriverWrapper.getDriver())
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(1))
+                .ignoring(NoSuchElementException.class)
+                .ignoring(NoAlertPresentException.class)
+                .withMessage("Web Element is not found within 30 seconds");
+
+        WebElement element = (WebElement) fWait.until(new Function<WebDriver, WebElement>() {
+            public WebElement apply(WebDriver driver) {
+                return driver.findElement(locator);
+            }
+        });
+        return element;
+    }
+
+    public List<WebElement> findElements_fluentWait(By locator) {
+        Wait fWait = new FluentWait(DriverWrapper.getDriver())
+                .withTimeout(Duration.ofSeconds(30))
+                .pollingEvery(Duration.ofSeconds(1))
+                .ignoring(NoSuchElementException.class)
+                .ignoring(NoAlertPresentException.class)
+                .withMessage("Web Element is not found within 30 seconds");
+
+        List<WebElement> element = (List<WebElement>) fWait.until(new Function<WebDriver, List<WebElement>>() {
+            public List<WebElement> apply(WebDriver driver) {
+                return driver.findElements(locator);
+            }
+        });
+        return element;
+    }
 
 
 
